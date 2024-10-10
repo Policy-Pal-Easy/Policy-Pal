67729f5254caf6a96e8925c2745aab482e6ab5bb
Sanket Koirala <koiralasanket11@gmail.com>
2024-10-10 02:09:34 -0500

    Landing page component

---
+++ src/app/fullpage/fullpage.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fullpage',
+  templateUrl: './fullpage.component.html',
+  styleUrls: ['./fullpage.component.css']
+})
+export class FullpageComponent {
+isCollapsed1 = true;
+  isCollapsed2 = true;
+  isCollapsed3 = true;
+  isCollapsed4 = true;
+  isCollapsed5 = true;
+}
